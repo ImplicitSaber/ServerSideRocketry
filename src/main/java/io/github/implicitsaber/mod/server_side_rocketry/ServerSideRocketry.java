c5b3e355b4477950805e4d1e1ca3915f27364d78
Jacob Rutledge <jacob.rutledge0509@gmail.com>
2025-12-22 00:41:23 -0600

    Initial commit

---
+++ src/main/java/io/github/implicitsaber/mod/server_side_rocketry/ServerSideRocketry.java
@@ -0,0 +1,14 @@
+package io.github.implicitsaber.mod.server_side_rocketry;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ServerSideRocketry implements ModInitializer {
+
+    public static final String MOD_ID = "server_side_rocketry";
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
